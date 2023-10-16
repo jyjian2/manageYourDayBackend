66c72947d664459de710d3ce0d8c3ce4ba6efe22
Jia-Yi Jian <jyjian@illinois.edu>
2023-10-16 15:03:29 -0700

    added temporary data ad JSON format

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const http = require('http');
 
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        important: true
+    },
+    {
+        id: 2,
+        content: "Browser can execute only JavaScript",
+        important: false
+    },
+    {
+        id: 3,
+        content: "ET and POST are the most important methods of HTTP protocol",
+        important: true
+    },
+]
+
+
+//use createServer mrthod to create a web server
+//an event handler is called every time a HTTP request is made to the server's address
 const app = http.createServer((request, response) => {
     response.writeHead(200, {'Content-Type': 'text/plain'})
     response.end('Hello World')
 })
 
+
+//bond HTTP server to listen to HTTP request sent to port 3001
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server is running on ${PORT}`)
