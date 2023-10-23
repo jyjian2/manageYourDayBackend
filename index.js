cb50af2f5915c0384b84c3805140bb1cf6bc44eb
Jia-Yi Jian <jyjian@illinois.edu>
2023-10-23 15:13:23 -0700

    refined post request method

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json())
+
 let notes = [
     {
         id: 1,
@@ -41,6 +43,40 @@ app.get('/api/notes/:id', (request, response) => {
         response.send("No note is found")
         response.status(404).end()
     }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id);
+    console.log(id)
+    notes = notes.filter(n => n.id !== id)
+    response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+
+    //The json-parser takes the JSON data of a request, 
+    //transforms it into a JavaScript object 
+    //and then attaches it to the body property of the request object 
+    const body = request.body
+
+    const generateId = () => {
+        return notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0;
+    }
+    
+    if (!body) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId(),
+    }
+
+    notes = notes.concat(note)
+    response.json(note)
 })
 
 //bond HTTP server to listen to HTTP request sent to port 3001
